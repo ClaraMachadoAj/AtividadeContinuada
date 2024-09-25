c7743f4b3ce45c7cf20b62719cd2b77b3809e829
ClaraMachadoAj <claramachado2005@gmail.com>
2024-09-25 09:45:02 -0300

    Classe "Acao" finalizada

--- src/main/java/br/com/cesarschool/poo/titulos/entidades/Acao.java
+++ src/main/java/br/com/cesarschool/poo/titulos/entidades/Acao.java
@@ -1,4 +1,5 @@
 package br.com.cesarschool.poo.titulos.entidades;
+import java.time.LocalDate;
 /*
  * Esta classe deve herdar de Ativo.
  * E deve ter os seguintes atributos:
@@ -10,6 +11,30 @@ package br.com.cesarschool.poo.titulos.entidades;
  * Deve ter um metodo público double calcularPrecoTransacao(double montante): o preço
  * da transação é o produto do montante pelo valorUnitario.
  */
-public class Acao {
 
+public class Acao extends Ativo {
+
+    // Atributo valorUnitario, do tipo double
+    private double valorUnitario;
+
+    // Construtor público que inicializa os atributos
+    public Acao(int identificador, String nome, LocalDate dataDeValidade, double valorUnitario) {
+        super(identificador, nome, dataDeValidade); // Chama o construtor da classe pai (Ativo)
+        this.valorUnitario = valorUnitario;
+    }
+
+    // Metodo set para valorUnitario
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    // Metodo get para valorUnitario
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    // Metodo público calcularPrecoTransacao(double montante)
+    public double calcularPrecoTransacao(double montante) {
+        return montante * valorUnitario;
+    }
 }
