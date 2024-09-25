cb982f68b4a64280d6970a90b97048d5fb4951a9
ClaraMachadoAj <claramachado2005@gmail.com>
2024-09-25 09:26:40 -0300

    commit incializando

--- src/main/java/br/com/cesarschool/poo/titulos/entidades/Ativo.java
+++ src/main/java/br/com/cesarschool/poo/titulos/entidades/Ativo.java
@@ -2,6 +2,17 @@ package br.com.cesarschool.poo.titulos.entidades;
 
 import java.time.LocalDate;
 
+/*
+ * Esta classe deve ter os seguintes atributos:
+ * identificador, do tipo int
+ * nome, do tipo String
+ * data de validade, do tipo LocalDate
+ *
+ * Deve ter um construtor público que inicializa os atributos,
+ * e métodos set/get públicos para os atributos. O atributo identificador
+ * é read-only fora da classe.
+ */
+
 public class Ativo {
     private int identificador;
     private String nome;
